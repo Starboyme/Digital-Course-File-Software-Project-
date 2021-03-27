2694b5ba59efe2dd1a81782550fbb1e35f37d083
rajpradeep01 <67677610+rajpradeep01@users.noreply.github.com>
2021-03-27 12:09:56 +0530

    Push-7

--- test.py
+++ test.py
@@ -2,75 +2,97 @@ GlowScript 3.0 VPython
 
 cwidth=800
 cheight=600
-bwidth=100
-bheight=50
-scene=canvas(width=cwidth,height=cheight)
+scene=canvas(width=cwidth,height=cheight,background=color.black)
 
-heightofline=30
-widthofline=1
-sizeofbox=10
+li=100
+hi=hv=50
+wi=wv=1
+lv=200
 
-dic={}
-#dic[1]="a"
-#dic[2]="b"
-#dic[3]={}
-#dic[3][1]="r"
-#dic[3][2]={}
-#dic[3][3]="raj"
-#dic[3][2][1]="p"
-#dic[3][2][2]={}
-#dic[3][2][2][1]="a"
-#dic[3][2][2][2]="b"
-#dic[3][2][2][3]="c"
-#dic[3][2][3]="thukka"
-#dic[4]="c"
-#dic[5]="c"
-
-d1=box(pos=vec(-300,200,0),color=color.red,size=vec(sizeofbox,sizeofbox,sizeofbox))
+htxt=15
 
 global org
-org={'x':-300,'y':200,'z':0}
+org={'x':-300,'y':200,'z':1}
 
-def anime(dic):
-    flag=0
-    for i in dic:
-        l = box(pos=vec(org['x'],org['y']-(heightofline/2),org['z']),axis=vec(0,-1,0),size=vec(heightofline,widthofline,1),color=color.white)
-        r = box(pos=vec(org['x']+(heightofline/2),org['y']-heightofline,org['z']),axis=vec(1,0,0),size=vec(heightofline,widthofline,1),color=color.white)
-        b = box(pos=vec(org['x']+heightofline+5,org['y']-heightofline,org['z']),size=vec(sizeofbox,sizeofbox,sizeofbox),color=color.blue)
-        tempy=org['y']-heightofline
-        if(type(dic[i]) is type({})):
-            b.color=color.red
-            org['x']+=heightofline+5
-            org['y']-=heightofline
-            anime(dic[i])
-            org['x']-=heightofline+5
-            flag=1
-        org['y']-=heightofline
-        if flag==1:
-            box(pos=vec(org['x'],org['y']+((tempy-org['y'])/2),org['z']),axis=vec(0,-1,0),size=vec(tempy-org['y'],widthofline,1),color=color.white)
-            flag=0
+d1=box(pos=vec(org['x'],org['y'],org['z']),color=color.blue,size=vec(li,hi,wi))
+d2=box(pos=vec(org['x']+li/2+lv/2+2,org['y'],org['z']),color=color.red,size=vec(lv,hv,wv))
+t1=text(text='Index',align='center', color=color.black, pos=vec(org['x'],org['y'],org['z']),height=htxt)
+t2=text(text='Value',align='center', color=color.black, pos=vec(org['x']+li/2+lv/2+2,org['y'],org['z']),height=htxt)
+
+coordinatelist=[]
+txtlist=[]
+boxlist=[]
+temp=org['y']
+for i in range(1,10):
+    temp-=(hi+1)
+    box(pos=vec(org['x'],temp,org['z']),color=color.blue,size=vec(li,hi,wi))
+    text(text=i,align='center', color=color.black, pos=vec(org['x'],temp,org['z']),height=htxt)
+    txtlist.append([])
+    boxlist.append([])
+    coordinatelist.append([])
+    boxlist[i-1].append(box(pos=vec(org['x']+li/2+lv/2+2,temp,org['z']),color=color.red,size=vec(lv,hv,wv)))
+    txtlist[i-1].append(text(text=".",align='center', color=color.black, pos=vec(org['x']+li/2+lv/2+2,temp,org['z']),height=htxt))
+    coordinatelist[i-1].append({'x':org['x']+li/2+lv/2+2,'y':temp,'z':org['z']})
+    
+#txtlist[2].visible=False
+#text(text="raj",align='center', color=color.black, pos=vec(coordinatelist[2]['x'],coordinatelist[2]['y'],coordinatelist[2]['z']),height=htxt)
+
+def getinput():
+    key=int(input("Enter the key: "))
+    value=input("Enter the value: ")
+    hash=key%10-1
+    if(txtlist[hash][0].text=="."):
+        txtlist[hash][0].visible=False
+        txtlist[hash][0]=text(text="key: "+str(key)+",value: "+str(value),align='center', color=color.black, pos=vec(coordinatelist[hash][0]['x'],coordinatelist[hash][0]['y'],coordinatelist[hash][0]['z']),height=htxt)
+    else:
+        endid=len(coordinatelist[hash])-1
+        coord=vec(coordinatelist[hash][endid]['x']+lv+2,coordinatelist[hash][endid]['y'],coordinatelist[hash][endid]['z'])
+        boxlist[hash].append(box(pos=coord,color=color.red,size=vec(lv,hv,wv)))
+        txtlist[hash].append(text(text="key: "+str(key)+",value: "+str(value),align='center', color=color.black, pos=coord,height=htxt))
+        coordinatelist[hash].append({'x':coordinatelist[hash][endid]['x']+lv+2,'y':coordinatelist[hash][endid]['y'],'z':coordinatelist[hash][endid]['z']})
+    
+def deletevalue():
+    key=int(input("Enter the key: "))
+    hash=key%10-1
+    for i in range(len(txtlist[hash])):
+        if(int(txtlist[hash][i].text.substring( 5, txtlist[hash][i].text.indexOf(",")))==key):
+            boxlist[hash][i].visible=False
+            coordinatelist[hash][i].visible=False
+            txtlist[hash][i].visible=False
+            coordinatelist[hash].pop()
+            boxlist[hash].pop()
+            txtlist[hash].pop()
+    
+def searchvalue():
+    key=int(input("Enter the key: "))
+    hash=key%10-1
+    for i in range(len(txtlist[hash])):
+        if(int(txtlist[hash][i].text.substring( 5, txtlist[hash][i].text.indexOf(",")))==key):
+            boxlist[hash][i].color=vec(0,1,0)
+            print(txtlist[hash][i].text)
+            sleep(2)
+            boxlist[hash][i].color=color.red
         
-def insert():  
-    for i in range(5):
-        key=int(input("Key: "))
-        value=int(input("Value: "))
-        dic[key]=value
-
-def display():
-    anime(dic)
+    
+        
 
 while True:
     insertion=box(pos=vec(260,-200,0),size=vec(80,40,1),color=color.red)
-    display=box(pos=vec(260,-140,0),size=vec(80,40,1),color=color.blue)
+    deletion=box(pos=vec(260,-100,0),size=vec(80,40,1),color=color.red)
+    search=box(pos=vec(260,0,0),size=vec(80,40,1),color=color.red)
+    text(text='Insert',align='center', color=color.black, pos=vec(260,-200,0),height=htxt)
+    text(text='Delete',align='center', color=color.black, pos=vec(260,-100,0),height=htxt)
+    text(text='Search',align='center', color=color.black, pos=vec(260,0,0),height=htxt)
     ev = scene.waitfor('click')
     if ev.event == 'click':
         print(scene.mouse.pos)
         t=scene.mouse.pos
-        if t.x>=220 and t.x<=300 and t.y>=-220 and t.x<=-180:
-            insert()
-        elif t.x>=220 and t.x<=300 and t.y>=-160 and t.y<=-120:
-            display()
-        
-    
+        if t.x>=220 and t.x<=300 and t.y>=-220 and t.y<=-180:
+            getinput()
+        elif t.x>=220 and t.x<=300 and t.y>=-120 and t.y<=-80:
+            deletevalue()
+        elif t.x>=220 and t.x<=300 and t.y>=-80 and t.y<=20:
+            searchvalue()
+        else:
     
 
--- test1.js
+++ test1.js
@@ -1,34 +1,13 @@
-var express = require('express');
-var app = express();
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
-var con = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "password",
-  database: "digitalcoursefile_db"
-});
-
-app.get('/login', function (req, res) {
-    
-  con.connect(function(err){
-      con.query(`select * from login`,function(err,results){
-          console.log(results);
-          res.send("nothing");
-      });
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = { name: "Ocean Tex", address: "Kamarajapuram" };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
   });
 });
-
-console.log("hello");
-
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   con.query("CREATE DATABASE mydb", function (err, result) {
-//     if (err) throw err;
-//     console.log("Database created");
-//   });
-// });
-
-var server = app.listen(8080, function () {});
